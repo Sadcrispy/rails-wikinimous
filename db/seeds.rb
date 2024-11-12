0d416eb58fdd294d971f9df28fe4e9c4c90cb3b1
Bartek Mikorski <bart@googency.com>
2024-11-12 15:51:35 +0900

    woah

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  Article.new(title: Faker::FunnyName.name, content: Faker::Quote.famous_last_words).save
+end
